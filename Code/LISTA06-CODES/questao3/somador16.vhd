f3133476ac47966c684b257aec6389283e3b4850
Guilherme Bernardo <bguilherme51@gmail.com>
2021-03-22 18:33:32 -0400

    questao 3 realizada e testada

---
+++ Code/LISTA06-CODES/questao3/somador16.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity somador16 is
+    port(
+        in_portA : in STD_LOGIC_VECTOR(15 downto 0);
+        in_portB : in STD_LOGIC_VECTOR(15 downto 0);
+        out_port : out STD_LOGIC_VECTOR(15 downto 0)
+    );
+end somador16;
+
+architecture behavior of somador16 is
+begin
+    out_port <= in_portA + in_portB;
+end behavior;
